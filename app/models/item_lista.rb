985ee1bdd6de9f1e0b98e54eb1c935b3e2685502
juan <juanfariastk@gmail.com>
2025-03-17 22:34:30 -0300

    fix: missing item lista message

--- app/models/item_lista.rb
+++ app/models/item_lista.rb
@@ -2,5 +2,5 @@ class ItemLista < ApplicationRecord
   belongs_to :lista_compra
 
   validates :nome_item, presence: true
-  validates :quantidade, numericality: { greater_than: 0 }
+  validates :quantidade, numericality: { greater_than: 0, message:"O item não pode ter quantidade igual ou menor que zero." }
 end
